0e3f488d1164dca38b4d724f78c81395e163a10e
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-04-12 12:53:50 -0400

    set up a checkDatabase.  This will check the database in order to see if there is any info that can be displayed.  once the database is checked for the info the info is fetched and then displayed.  it also will check to see if anything is stored.  it will display that similarly.

--- public/db.js
+++ public/db.js
@@ -7,3 +7,41 @@ request.onupgradeneeded = function(event){
     db.createObjectStore("pending", { autoIncrement: true});
 };
 
+request.onsuccess = function(event){
+    db = event.target.result;
+
+    if (navigator.onLine){
+        checkDatabase();
+    }
+}
+
+function checkDatabase() {
+
+  const transaction = db.transaction(["pending"], "readwrite");
+  
+  const store = transaction.objectStore("pending");
+  
+  const getAll = store.getAll();
+  
+getAll.onsucess = function(){
+    if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(() => {
+            const transaction = db.transaction(["pending"], "readwrite");
+
+            const store = transaction.objectStore("pending");
+
+            store.clear()
+        });
+
+    }
+}
+}
